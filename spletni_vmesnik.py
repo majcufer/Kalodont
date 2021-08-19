3d9b359227b834343e849df76fbd7c058cec2963
Maj <maj.cufer@gmail.com>
2021-08-19 16:58:54 +0200

    dodan spletni vmesnik in pripadujoče datoteke

---
+++ spletni_vmesnik.py
@@ -0,0 +1,37 @@
+import bottle
+import model
+
+SKRIVNOST = 'upamdabotolenakoncudelal'
+kalodont = model.Kalodont('stanje.json')
+
+
+@bottle.get("/")
+def osnovna_stran():
+    return bottle.template('Kalodont/views/osnovna_stran.html')
+
+
+@bottle.post("/nova_igra/")
+def nova_igra():
+    id_igre = kalodont.nova_igra()
+    bottle.response.set_cookie('idigre', id_igre, secret=SKRIVNOST, path='/')
+    bottle.redirect("/igra/")
+
+
+@bottle.get("/igra/")
+def pokazi_igro():
+    id_igre = bottle.request.get_cookie('idigre', secret=SKRIVNOST)
+    (igra, odziv) = kalodont.igre[id_igre]
+    return bottle.template('Kalodont/views/igra.html',
+                           id_igre=id_igre,
+                           igra=igra,
+                           odziv=odziv)
+
+@bottle.post("/igra/")
+def ugibaj():
+    id_igre = bottle.request.get_cookie('idigre', secret=SKRIVNOST)
+    beseda = bottle.request.forms.getunicode("beseda")
+    kalodont.vnasanje(id_igre, beseda)
+    bottle.redirect("/igra/")
+
+
+bottle.run(reloader=True, debug=True)
